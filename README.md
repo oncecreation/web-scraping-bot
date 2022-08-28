a5211e2c8aa2d7ecc5432485155ed05840f200db
oncecreation <jameshussey474@gmail.com>
2022-08-28 22:40:38 +0300

    add readme. test-scrape with selenium and request.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# web-scraping-bot
- automate extraction of news from google, bing or other sources about specific topics
+# Background
+This project will evolve gradually into a pre-cursor for the [text-summarizer project](https://github.com/prak112/text-summarizer). Text output from this project will be summarized using the text-summarizer.
+ 
+In test-run, we aggregate example tutorials from [GeekForGeeks](https://www.geeksforgeeks.org/how-to-build-web-scraping-bot-in-python/) and [freeCodeCamp](https://www.freecodecamp.org/news/automate-your-life-with-python/) to automate extraction of news from Google or Bing.
+</br></br>
+
+# Aim 
+(*Idea in Progress, bound to change*)
+- Test-run the Web scraper-bot idea using tutorial blog sources.
+- Build Web scraper-bot to extract news based on a specific topic, whenever required.</br></br>
+
+# Sources
+### Blogs
+- [GeekForGeeks](https://www.geeksforgeeks.org/how-to-build-web-scraping-bot-in-python/)
+- [freeCodeCamp](https://www.freecodecamp.org/news/automate-your-life-with-python/)
+
+### Tools & Libraries
+- Python 3.9.12
+- Requests 2.0.12
+- Selenium 4.4.3 /& BeautifulSoup (bs4 4.11.1)
+- time, datetime (Python built-in)
+</br></br>
+
+# Approach
+(*Idea in Progress, bound to change*)
+- 
+- 
---
+++ test-run/news-scraper.py
@@ -0,0 +1,52 @@
+#%%
+# import libraries
+import selenium
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+
+print("All libraries imported successfully.")
+
+# %%
+# set selenium webdriver PATH - https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/
+# using Driver Management Software
+# selenium version 4+
+
+import selenium
+import webdriver_manager
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+# initialise --- webdriver not detected--error
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+# run driver for url
+url = "https://yle.fi/news"
+driver.get(url)
+
+# provide full XPath of top article
+news_path = "/html/body/div[1]/div[2]/div/div[1]/div[1]/a"
+link = driver.find_element(news_path)
+
+# get headline --time out error
+print(link.text)
+
+# %%
+# use requests for YLE
+url = "https://yle.fi/news"
+
+# get html content
+r =  requests.get(url)
+text = r.text
+
+# make soup and filter list items
+soup = BeautifulSoup(text, features='lxml')
+items = soup.find_all('li')
+
+# iterate list and collect items in list
+all_items = []
+for item in items:
+    all_items.append(item.text)
+
+print("Total items extracted, including headlines : \n", len(all_items))
+# %%
