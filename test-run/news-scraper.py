f28fad3fde0c30a06454c1d1b3ec47cdac100f8f
oncecreation <jameshussey474@gmail.com>
2022-11-21 17:54:47 +0200

    choose requests. filter by class.

--- test-run/news-scraper.py
+++ test-run/news-scraper.py
@@ -1,57 +1,30 @@
 #%%
 # import libraries
-import selenium
-from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
 
 print("All libraries imported successfully.")
 
 # %%
-# USING SELENIUM
-# set selenium webdriver PATH - https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/
-# using Driver Management Software
-# selenium version 4+
-
-import selenium
-import webdriver_manager
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
-# initialise --- webdriver not detected--error
-driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
-
-# run driver for url
-url = "https://yle.fi/news"
-driver.get(url)
-
-# provide full XPath of top article
-news_path = "/html/body/div[1]/div[2]/div/div[1]/div[1]/a"
-link = driver.find_element(news_path)
-
-# get headline --time out error
-print(link.text)
-
-# %%
-# USING REQUESTS
-# use requests for YLE
-import requests
-from bs4 import BeautifulSoup
-
+# webpage url
 url = "https://yle.fi/news"
 
 # get html content
 r =  requests.get(url)
 text = r.text
 
-# make soup and filter list items
+# make soup and filter list items  
 soup = BeautifulSoup(text, features='lxml')
-items = soup.find_all('li')
+
+# filter soup by class for 'Most Read' news items
+s = soup.find('div', class_= 'MinimalHeadlines__Container-sc-zhu28g-0 kLTbWH')
+content = s.find_all('a')
 
 # iterate list and collect items in list
 all_items = []
-for item in items:
+for item in content:
     all_items.append(item.text)
 
 print("Total items extracted, including headlines : \n", len(all_items))
 # %%
+# Parse 'Most Read' news items into CSV 
